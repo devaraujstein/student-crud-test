d60881b50a793a36cf08e9e7667f61fcceff2f9a
Andre Araujo <andreearaujo98@gmail.com>
2021-02-20 20:18:38 -0300

    remove

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList, Image } from 'react-native';
 
 import styles from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import database from '@react-native-firebase/database';
 
 const Main = () => {
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
+    const [listPupil, setListPupil] = useState([]);
+
 
     const pushFire = () => {
         try {
@@ -18,11 +22,42 @@ const Main = () => {
         } catch (error) {
             console.log(error);
         }
-        finally{
+        finally {
             setName('');
             setAddress('');
         }
     }
+
+    const editFire = (key) => {
+        console.log('Edite: ', key);
+    }
+
+    const deleteFire = (key) => {
+        try{
+            database().ref(`pupil/${key}`).remove();
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        try {
+            database().ref('pupil').on('value', snapshot => {
+                const list = [];
+
+                snapshot.forEach(item => {
+                    list.push({
+                        key: item.key,
+                        name: item.val().name,
+                        address: item.val().address
+                    });
+                });
+                setListPupil(list);
+            });
+        }catch(error){
+            console.log(error);
+        }
+    }, []);
 
     return (
         <>
@@ -44,6 +79,35 @@ const Main = () => {
                 <TouchableOpacity style={styles.button} onPress={pushFire}>
                     <Text style={styles.text} >Cadastrar Aluno</Text>
                 </TouchableOpacity>
+
+                <View style={{ margin: 8 }} />
+
+                <FlatList 
+                    data={listPupil}
+                    keyExtractor={item => item.key}
+                    renderItem={({item}) => 
+
+                    <View style={styles.listContainer}>
+                        <Image style={styles.avatar} source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} />
+                        <View>
+                            <Text style={styles.textName} >{item.name}</Text>
+                            <Text style={styles.textAddress} >{item.address}</Text>
+                        </View>
+
+                        <View style={styles.listUD}>
+                            <TouchableOpacity onPress={() => editFire(item.key)}>
+                                <Icon name="account-edit" size={40} color="#6BA0FC" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => deleteFire(item.key)}>
+                                <Icon name="delete-forever" size={40} color="#f44336" />
+                            </TouchableOpacity>
+                        </View>
+                        
+                    </View>
+
+                    }
+                />
+
             </View>
         </>
     )
